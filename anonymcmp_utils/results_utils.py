fix epsilon computation for NN differential privacy

anonymcmp_utils/results_utils.py
@@ -8,15 +8,13 @@ def save_results(fname_list, k_trials, acc_proc, acc_vanilla, npfname, yminmax_l
     ylabels = ['Model accuracy', 'Membership attack accuracy', 'Attribute blackbox attack accuracy',
                'Attribute whitebox attack accuracy 1', 'Attribute whitebox attack accuracy 2']
 
-    legend_list = [True, False, False, False, False]
-
     for i in range(len(acc_vanilla)):
         accs_mes = {k: np.array(accs)[:, i] for k, accs in acc_proc.items()}
         plot_anoresult(fname_list[i], k_trials, ylabels[i], accs_mes, acc_vanilla[i],
-                       yminmax_list[i][0], yminmax_list[i][1], legend_list[i], epsilons)
+                       yminmax_list[i][0], yminmax_list[i][1], epsilons)
 
 
-def plot_anoresult(fname, k_trials, ylabel, acc_proc, acc_vanilla, ylim_min, ylim_max, legend, epsilons):
+def plot_anoresult(fname, k_trials, ylabel, acc_proc, acc_vanilla, ylim_min, ylim_max, epsilons):
     fig, ax = plt.subplots()
 
     markers = ['-s', '-^', '-o', '-<']
@@ -33,6 +31,7 @@ def plot_anoresult(fname, k_trials, ylabel, acc_proc, acc_vanilla, ylim_min, yli
 
     if 'Differential privacy' in acc_proc.keys():
         ax2 = ax.twiny()
+        ax2.set_xscale('log')
         ln_dp = ax2.plot(epsilons, acc_proc['Differential privacy'], '-v', fillstyle='none',
                          label="Differntial privacy", color='tab:purple')[0]
         ax2.set_xlabel("epsilons")
@@ -45,8 +44,17 @@ def plot_anoresult(fname, k_trials, ylabel, acc_proc, acc_vanilla, ylim_min, yli
     ax.set_ylabel(ylabel)
 
     labs = [l.get_label() for l in lns]
-    if legend:
-        ax.legend(lns, labs)
+
+    export_legend(ax.legend(lns, labs, loc=3, framealpha=1, frameon=True), fname.replace('.', '_legend.'))
 
     fig.tight_layout()
     plt.savefig(fname)
+
+
+def export_legend(legend, filename, expand=[-5,-5,5,5]):
+    fig = legend.figure
+    fig.canvas.draw()
+    bbox = legend.get_window_extent()
+    bbox = bbox.from_extents(*(bbox.extents + np.array(expand)))
+    bbox = bbox.transformed(fig.dpi_scale_trans.inverted())
+    fig.savefig(filename, dpi="figure", bbox_inches=bbox)

anonymcmp_utils/testers/anonym_NN_tester.py
@@ -3,6 +3,7 @@ import dp_accounting
 import numpy as np
 from scipy import interpolate
 from tensorflow_privacy.privacy.optimizers.dp_optimizer_keras import DPKerasAdamOptimizer
+from tensorflow_privacy.privacy.analysis.compute_dp_sgd_privacy import compute_dp_sgd_privacy
 from .anonym_tester import AnonymTester
 
 class AnonymNNTester(AnonymTester):
@@ -15,7 +16,9 @@ class AnonymNNTester(AnonymTester):
         self.epochs = epochs
 
         noise_multipliers = np.linspace(0.1, 1.0).tolist() + [pow(10, i) for i in range(1, 4)]
-        epsilons = [self.compute_epsilon(epochs*sample_len//batch_size, nm, sample_len, batch_size)
+
+        epsilons = [compute_dp_sgd_privacy(n=sample_len, batch_size=batch_size, noise_multiplier=nm, epochs=epochs,
+                                           delta=1 / pow(10, int(np.ceil(np.log10(sample_len)))))[0]
                     for nm in noise_multipliers]
 
         self.f = interpolate.interp1d(np.log(epsilons), np.log(noise_multipliers), kind='slinear', fill_value='extrapolate')
@@ -51,18 +54,3 @@ class AnonymNNTester(AnonymTester):
 
     def compute_noise_multiplier(self, eps):
         return np.e**self.f(np.log(eps))
-
-    def compute_epsilon(self, steps, nm, sample_len, batch_size):
-        if nm == 0.0:
-            return float('inf')
-        orders = [1 + x / 10. for x in range(1, 100)] + list(range(12, 64))
-        accountant = dp_accounting.rdp.RdpAccountant(orders)
-
-        sampling_probability = batch_size / sample_len
-        event = dp_accounting.SelfComposedDpEvent(
-            dp_accounting.PoissonSampledDpEvent(sampling_probability, dp_accounting.GaussianDpEvent(nm)), steps)
-
-        accountant.compose(event)
-
-        return accountant.get_epsilon(target_delta=1 / pow(10, int(np.ceil(np.log10(sample_len)))))
-