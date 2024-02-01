c9dff1d1623879432a8a5c5fba13a50ed913b1bc
Kaori Hagihara <kaori.hagihara@uclouvain.be>
2024-02-01 18:11:47 +0100

    change epsilon values of DP, set max_depth of decision tree

--- anonymcmp_utils/testers/anonym_DT_tester.py
+++ anonymcmp_utils/testers/anonym_DT_tester.py
@@ -5,12 +5,12 @@ import diffprivlib.models as dp
 from .anonym_tester import AnonymTester
 
 class AnonymDTTester(AnonymTester):
-    def __init__(self, attack_column, sensitive_column):
+    def __init__(self, attack_column, sensitive_column, max_depth):
         super(AnonymDTTester, self).__init__(attack_column, sensitive_column)
-
+        self.max_depth = max_depth
 
     def get_trained_model(self, x, y):
-        model = DecisionTreeClassifier()
+        model = DecisionTreeClassifier(max_depth=self.max_depth)
         model = model.fit(x, y)
 
         return model
@@ -31,4 +31,4 @@ class AnonymDTTester(AnonymTester):
             [np.argmax(arr) for arr in ScikitlearnDecisionTreeClassifier(model).predict(x)]).reshape(-1, 1)
 
     def get_diffpriv_classifier(self, eps):
-        return dp.DecisionTreeClassifier(epsilon=eps)
+        return dp.DecisionTreeClassifier(epsilon=eps, max_depth=self.max_depth)
--- tests/anonymization_adult-dectree.py
+++ tests/anonymization_adult-dectree.py
@@ -10,19 +10,21 @@ x_train_encoded = preprocessor.fit_transform(x_train)
 x_test_encoded = preprocessor.transform(x_test)
 
 k_trials = (50, 100, 200, 400, 800, 1000)
-epsilons = [0.01, 0.1, 0.5, 1.0, 5.0, 10.0, 25.0, 75.0, 100.0, 200.0]
+epsilons = [0.2, 0.25, 0.3, 0.35, 0.4, 0.5, 1.0, 2.5, 5.0, 7.5, 10.0]
 
 QI = ['age', 'education-num', 'race', 'relationship']
 
-tester = AnonymDTTester(attack_column='relationship', sensitive_column='relationship')
+tester = AnonymDTTester(attack_column='relationship', sensitive_column='relationship', max_depth=20)
 
 acc_vanilla, acc_proc = tester.perform_test(x_train, x_train_encoded, y_train, x_test_encoded, y_test, preprocessor,
-                                            QI, k_trials, epsilons)
+                                            QI, k_trials, epsilons, multitest_vanilla=True)
+
 
 plot_path = 'results/plots/'
 fname_base = 'anonymization_adult-dectree'
 imfname = fname_base + '.png'
-yminmax_list = [[0.6, 0.85], [0.55, 1.0], [0.55, 1.0], [0.55, 1.0], [0.55, 1.0]]
+yminmax_list = [[0.7, 0.9], [0.55, 1.0], [0.55, 1.0], [0.55, 1.0], [0.55, 1.0]]
+
 
 results_utils.save_results([plot_path+'inference/'+imfname,
                             plot_path+'member_attack/'+imfname,
