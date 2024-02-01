02b6fcce513ed757c1ffa240a1698d919cfda095
Kaori Hagihara <kaori.hagihara@uclouvain.be>
2024-02-01 22:35:54 +0100

    update the filename

--- run_testscript.sh
+++ run_testscript.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker run --rm --gpus all -v ${PWD}/tests/results:/tf/dev/tests/results $(cat IMAGE) \
- /bin/sh -c 'cd /tf/dev/tests; python anonymization_adult-levels-NN1.py'
+ /bin/sh -c 'cd /tf/dev/tests; python anonymization_adult-NN1.py'
