5c7a2b433a9cb0e2db177e75440c54ce3e3fc6ed
Kaori Hagihara <kaori.hagihara@uclouvain.be>
2024-03-13 12:28:07 +0100

    fix blackbox attack

--- tests/anonymization_nursery-dectree-QiAll.py
+++ tests/anonymization_nursery-dectree-QiAll.py
@@ -10,17 +10,18 @@ x_train_encoded = preprocessor.fit_transform(x_train)
 x_test_encoded = preprocessor.transform(x_test)
 
 k_trials = (50, 100, 200, 400, 800, 1000)
-epsilons = [0.01, 0.1, 0.5, 1.0, 5.0, 10.0, 25.0, 75.0, 100.0, 200.0]
 
 QI = ["parents", "has_nurs", "form", "children", "housing", "finance", "social", "health"]
 
-tester = AnonymDTTester(attack_column='social', sensitive_column='health')
+tester = AnonymDTTester(attack_column='social', sensitive_column='social', max_depth=15)
+
+fname_base = 'anonymization_nursery-dectree-QiAll'
 
 acc_vanilla, acc_proc = tester.perform_test(x_train, x_train_encoded, y_train, x_test_encoded, y_test, preprocessor,
-                                            QI, k_trials, epsilons)
+                                            QI, k_trials, multitest_vanilla=True,
+                                            model_path='results/models/'+fname_base)
 
 plot_path = 'results/plots/'
-fname_base = 'anonymization_nursery-dectree-QiAll'
 imfname = fname_base + '.png'
 
 yminmax_list = [[0.2, 1.0], [0.35, 1.0], [0.35, 1.0], [0.35, 1.0], [0.35, 1.0]]
@@ -30,4 +31,4 @@ results_utils.save_results([plot_path+'inference/'+imfname,
                             plot_path+'attrb_black_attack/'+imfname,
                             plot_path+'attrb_whiteLS_attack/'+imfname,
                             plot_path+'attrb_white_attack/'+imfname],
-                           k_trials, acc_proc, acc_vanilla, 'results/data/'+fname_base+'.npy', yminmax_list, epsilons)
+                           k_trials, acc_proc, acc_vanilla, 'results/data/'+fname_base+'.npy', yminmax_list)
