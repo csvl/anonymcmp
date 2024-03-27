dab4930c19eab97d315fd02c4ff89ad97639856f
Kaori Hagihara <kaori.hagihara@uclouvain.be>
2024-03-27 12:34:36 +0100

    change QI for anonym NN2 less samples

--- tests/anonymization_adult-NN2_less_samples.py
+++ tests/anonymization_adult-NN2_less_samples.py
@@ -16,13 +16,14 @@ def run_with_less_samples(dev_factor, fname_base, orig_x_train, orig_y_train):
 
     k_trials = (50, 100, 200, 400, 800, 1000)
     epsilons = [0.2, 0.25, 0.3, 0.35, 0.4, 0.5, 1.0, 2.5, 5.0, 7.5, 10.0]
-    QI = ['age', 'education-num', 'race', 'relationship']
+    QI = ['age', 'education-num', 'race', 'native-country']
 
     tester = AnonymGBClass2Tester(attack_column='relationship', sensitive_column='relationship',
                                   input_veclen=x_train_encoded.shape[1], sample_len=len(x_train))
 
     acc_vanilla, acc_proc = tester.perform_test(x_train, x_train_encoded, y_train, x_test_encoded, y_test, preprocessor,
-                                                QI, k_trials, epsilons, multitest_vanilla=True)
+                                                QI, k_trials, epsilons, multitest_vanilla=True,
+                                                model_path='results/models/' + fname_base)
 
     plot_path = 'results/plots/'
     imfname = fname_base + '.png'
@@ -35,9 +36,11 @@ def run_with_less_samples(dev_factor, fname_base, orig_x_train, orig_y_train):
                                k_trials, acc_proc, acc_vanilla, 'results/data/'+fname_base+'.npy', yminmax_list, epsilons)
 
 list_dev_factor = [2, 4, 8, 16]
-list_fname_base = ['anonymization_adult-NN2_quart_samples',
+list_fname_base = ['anonymization_adult-NN2_half_samples',
+                   'anonymization_adult-NN2_quart_samples',
                    'anonymization_adult-NN2_8th_samples',
-                   'anonymization_adult-NN2_16th_samples'] #'anonymization_adult-NN2_half_samples',
+                   'anonymization_adult-NN2_16th_samples']
+
 
 for dev_factor, fname_base in zip(list_dev_factor, list_fname_base):
     run_with_less_samples(dev_factor, fname_base, x_train, y_train)
