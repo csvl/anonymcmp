cfc3413a80d9fd81fae91cbede0b7a9bfdd71fa5
Kaori Hagihara <kaori.hagihara@uclouvain.be>
2024-04-12 15:59:03 +0200

    change QI for anonym NN2 more QIs

--- tests/anonymization_adult-NN2-Qi8_10_12.py
+++ tests/anonymization_adult-NN2-Qi8_10_12.py
@@ -11,17 +11,18 @@ x_train_encoded = preprocessor.fit_transform(x_train)
 x_test_encoded = preprocessor.transform(x_test)
 
 k_trials = (50, 100, 200, 400, 800, 1000)
-QIs = [['workclass', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country'],
-      ['age', 'workclass', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex',
-       'hours-per-week', 'native-country'],
-      ['age', 'workclass', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex',
-       'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']]
+QIs = [['age', 'education-num', 'marital-status', 'race', 'sex', 'native-country'],
+       ['age', 'workclass', 'education-num', 'marital-status', 'occupation', 'race', 'sex', 'native-country'],
+       ['age', 'workclass', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex',
+        'hours-per-week', 'native-country']]
 
 tester = AnonymGBClass2Tester(attack_column='relationship', sensitive_column='relationship',
                               input_veclen=x_train_encoded.shape[1], sample_len=len(x_train))
 
 acc_vanilla, acc_proc_list = tester.perform_anonymtest_QIs(x_train, x_train_encoded, y_train, x_test_encoded, y_test,
-                                                           preprocessor, QIs, k_trials)
+                                                           preprocessor, QIs, k_trials,
+                                                           'results/models/anonymization_adult-NN2/vanilla_0',
+                                                           model_root_path='results/models/anonymization_adult-NN2-Qi')
 
 plot_path = 'results/plots/'
 yminmax_list = [[0.6, 0.9], [0.55, 1.0], [0.55, 1.0], [0.55, 1.0], [0.55, 1.0]]
